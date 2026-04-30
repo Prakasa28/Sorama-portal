3ffbd18bf8bdadc941e95b2f200dd6734dde6083
Prakasa KS <prakasa.ks@sorama.eu>
2026-04-30 16:14:02 +0200

    feat: added search bar component

--- sorama-portal/components/measurementsOverview.tsx
+++ sorama-portal/components/measurementsOverview.tsx
@@ -1,19 +1,35 @@
 "use client";
 
+import { useState, useMemo } from "react";
 import { useWebRTC } from "../hooks/useWebRTC";
 import { MeasurementCard } from "./measurementCard";
+import { SearchBar } from "./searchBar";
 
 
 export function MeasurementsOverview() {
   const { folders } = useWebRTC();  
+   const [search, setSearch] = useState("");
+
+  const filteredFolders = useMemo(() => {
+    const searchValue = search.toLowerCase().trim();
+
+    if (!searchValue) return folders;
+
+    return folders.filter((folder) =>
+      folder.name.toLowerCase().includes(searchValue)
+    );
+  }, [folders, search]);
 
   return (
     <main className="measurements-page">
+        <SearchBar value={search} onChange={setSearch} placeholder="Search measurements..." />
         <section className="measurements-grid">
             {folders.length === 0 ? (
                 <p className="measurements-empty"> waiting for device data...</p>
-            ) : (
-                folders.map((folder) => (
+            ) :  filteredFolders.length === 0 ? (
+              <p className="measurements-empty">No measurements found.</p>
+            ) :(
+                filteredFolders.map((folder) => (
                     <MeasurementCard key={folder.name} folder={folder} />
                 ))
             )}
---
+++ sorama-portal/components/searchBar.tsx
@@ -0,0 +1,26 @@
+import { Search } from "lucide-react";
+
+type Props = {
+  value: string;
+  placeholder?: string;
+  onChange: (value: string) => void;
+};
+
+export function SearchBar({
+  value,
+  placeholder = "Search...",
+  onChange,
+}: Props) {
+  return (
+    <div className="search-bar">
+      <Search className="search-bar__icon" />
+
+      <input
+        className="search-bar__input"
+        value={value}
+        placeholder={placeholder}
+        onChange={(event) => onChange(event.target.value)}
+      />
+    </div>
+  );
+}
